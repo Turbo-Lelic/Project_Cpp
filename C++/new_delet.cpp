5b80266a682196ab668cdc00fb11758d5a24c1f5
Turbo-Lelic <alexey.alekseev.m@yandex.ru>
2023-02-24 19:02:26 +0300

    add file

--- C++/new_delet.cpp
+++ C++/new_delet.cpp
@@ -3,16 +3,23 @@
 
 using namespace std;
 
-int main(void) {
+signed main() {
 	setlocale(LC_ALL, "Russian");
 
-	int *ptr = new int;
-	*ptr = 10;
+	int *ptr_10 = new int;
+	int *ptr_arr = new int[5];
 
-	cout << "—сылка - " << ptr << "\n";
-	cout << "«начение - " << *ptr << "\n";
+	*ptr_10 = 10;
+	for (int i = 0; i < 5; i++) {
+		ptr_arr[i] = i + 1;
+		cout << "&ptr_arr[" << i << "] = " << &ptr_arr[i] << endl;
+		cout << "*ptr_arr[" << i << "] = " << ptr_arr[i] << endl;
+	}
+	cout << "&ptr_10 = " << &ptr_10 << endl;
+	cout << "*ptr_10 = " << *ptr_10 << endl;
 
-	delete ptr;
+	delete ptr_10; ptr_10 = nullptr;
+	delete[0] ptr_arr; ptr_arr = nullptr;
 
 	return 0;
 }
--- Wolfenmem/Wolfenmem.cpp
+++ Wolfenmem/Wolfenmem.cpp
@@ -5,6 +5,7 @@
 #include <chrono>
 #include <stdio.h>
 #include <Windows.h>
+#include <locale>
 
 using namespace std;
 
@@ -17,92 +18,94 @@ float fPlayerY = 8.0f;          // Стартовая позиция игрока Y
 float fPlayerA = 0.0f;			// Угол обзора
 float fFOV = 3.0f / 4.0f;	    // Поле зрения
 float fDepth = 16.0f;			// Максимальное расстояние зрения
-float fSpeed = 3.5f;			// Скорость ходьбы
+float fSpeed = 3.0f;			// Скорость ходьбы
 
 signed main() {
+	setlocale(LC_ALL, "Russian");
+
 	// экранный буфер
-	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
-	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-	SetConsoleActiveScreenBuffer(hConsole);
-	DWORD dwBytesWritten = 0;
+	wchar_t *screen = new wchar_t[nScreenWidth * nScreenHeight]; // Массив для записи в буфер
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL); // Буфер экрана
+	SetConsoleActiveScreenBuffer(hConsole); // Отображение буфера
+	DWORD dwBytesWritten = 0; // Дебаг
 
 	// Создать карту мирового пространства # = настенный блок, . = пробел
 	wstring map;
-	map += L"################";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
+	map += L"##########.#####";
+	map += L"#......#.#.....#";
+	map += L"#......#.####..#";
+	map += L"#......#.#.....#";
+	map += L"#...#..#.#.....#";
+	map += L"#......#.#.....#";
+	map += L"#..#...#.#.##..#";
+	map += L"#......#.#.....#";
+	map += L"#...#....#.....#";
+	map += L"#......#.#.....#";
+	map += L"########.#.....#";
+	map += L"#......#.#.....#";
+	map += L"#..##..#.####.##";
+	map += L"#..##..........#";
+	map += L"#......#.#######";
 	map += L"################";
 
-	auto tp1 = chrono::system_clock::now(); // Переменные для подсчета
-	auto tp2 = chrono::system_clock::now(); // пройденного времени
+	auto tp1 = chrono::system_clock::now(); // Переменные для подсчета пройденного времени
+	auto tp2 = chrono::system_clock::now(); // Тоже
 
 	while (true) {
-		// Нам понадобится разница во времени для каждого кадра, чтобы вычислить модификацию.
+		// понадобится разница во времени для каждого кадра, чтобы вычислить модификацию
 		// к скорости движения, чтобы обеспечить последовательное движение, как трассировка лучей
-		// недетерминирован
 		tp2 = chrono::system_clock::now();
 		chrono::duration<float> elapsedTime = tp2 - tp1;
 		tp1 = tp2;
 		float fElapsedTime = elapsedTime.count();
 
+		if (GetAsyncKeyState((unsigned short)VK_LSHIFT) & 0x8000 || GetAsyncKeyState((unsigned short)VK_RSHIFT) & 0x8000) { // Ускорение движения на LShift или RShift
+			fSpeed = 5.5;
+		} else { // Обычное движение
+			fSpeed = 3.5;
+		}
 
-		// Поворот против часовой стрелки
-		if (GetAsyncKeyState((unsigned short)'A') & 0x8000) {
+		if (GetAsyncKeyState((unsigned short)'A') & 0x8000) { // Поворот против часовой стрелки
 			fPlayerA -= (fSpeed * 0.75f) * fElapsedTime;
 		}
 
-		// Поворот по часовой стрелке
-		if (GetAsyncKeyState((unsigned short)'D') & 0x8000) {
+		if (GetAsyncKeyState((unsigned short)'D') & 0x8000) { // Поворот по часовой стрелке
 			fPlayerA += (fSpeed * 0.75f) * fElapsedTime;
 		}
 
-		// Движение вперед
-		if (GetAsyncKeyState((unsigned short)'W') & 0x8000) {
-			fPlayerX += sinf(fPlayerA) * fSpeed * fElapsedTime;;
-			fPlayerY += cosf(fPlayerA) * fSpeed * fElapsedTime;;
-
-			if (map.c_str()[(int)fPlayerX * nMapWidth + (int)fPlayerY] == '#') {
-				fPlayerX -= sinf(fPlayerA) * fSpeed * fElapsedTime;;
+		if (GetAsyncKeyState((unsigned short)'W') & 0x8000) { // Движение вперед
+			fPlayerX += sinf(fPlayerA) * fSpeed * fElapsedTime;
+			fPlayerY += cosf(fPlayerA) * fSpeed * fElapsedTime;
+			  
+			if (map.c_str()[(int)fPlayerX * nMapWidth + (int)fPlayerY] == '#') { // Если столкнулись со стеной, то откатываем шаг
+				fPlayerX -= sinf(fPlayerA) * fSpeed * fElapsedTime;
 				fPlayerY -= cosf(fPlayerA) * fSpeed * fElapsedTime;
 			}
 		}
 
-		// Движением назад
-		if (GetAsyncKeyState((unsigned short)'S') & 0x8000) {
-			fPlayerX -= sinf(fPlayerA) * fSpeed * fElapsedTime;;
-			fPlayerY -= cosf(fPlayerA) * fSpeed * fElapsedTime;;
+		if (GetAsyncKeyState((unsigned short)'S') & 0x8000) { // Движением назад
+			fPlayerX -= sinf(fPlayerA) * fSpeed * fElapsedTime;
+			fPlayerY -= cosf(fPlayerA) * fSpeed * fElapsedTime;
 
 			if (map.c_str()[(int)fPlayerX * nMapWidth + (int)fPlayerY] == '#') { // Если столкнулись со стеной, то откатываем шаг
-				fPlayerX += sinf(fPlayerA) * fSpeed * fElapsedTime;;
-				fPlayerY += cosf(fPlayerA) * fSpeed * fElapsedTime;;
+				fPlayerX += sinf(fPlayerA) * fSpeed * fElapsedTime;
+				fPlayerY += cosf(fPlayerA) * fSpeed * fElapsedTime;
 			}
 		}
 
 		for (int x = 0; x < nScreenWidth; x++) {
-			// Для каждого столбца вычислите угол проецируемого луча в мировое пространство.
+			// Для каждого столбца вычислите угол проецируемого луча в пространство.
 			float fRayAngle = (fPlayerA - fFOV / 2.0f) + ((float)x / (float)nScreenWidth) * fFOV;
 
 			// Найдите расстояние до стены
 			float fStepSize = 0.1f; // Размер приращения для raycasting, от уменьшения к увеличению									
 			float fDistanceToWall = 0.0f; // разрешение
 
-			bool bHitWall = false; // Установите, когда луч попадает в стену
-			bool bBoundary = false; // Установите, когда луч попадает на границу между двумя стеновыми блоками
+			bool bHitWall = false; // Установить, когда луч попадает в стену
+			bool bBoundary = false; // Установить, когда луч попадает на границу между двумя стенами
 
-			float fEyeX = sinf(fRayAngle); // Единичный вектор для луча в пространстве игрока
-			float fEyeY = cosf(fRayAngle);
+			float fEyeX = sinf(fRayAngle); // Единичный вектор для луча в пространстве игрока X
+			float fEyeY = cosf(fRayAngle); // Единичный вектор для луча в пространстве игрока Y
 
 			// Постепенно отбрасывать луч от игрока вдоль угла луча, проверяя
 			// пересечение с блоком
@@ -121,18 +124,16 @@ signed main() {
 					if (map.c_str()[nTestX * nMapWidth + nTestY] == '#') {
 						// луч ударился о стену
 						bHitWall = true;
-
 						// Чтобы выделить границы плитки, проведите луч из каждого угла
 						// плитки игроку. Чем больше совпадает этот луч
 						// к лучу рендеринга, тем ближе мы к тайлу
 						// граница, которую мы заштрихуем, чтобы добавить деталей стенам
 						vector<pair<float, float>> p;
-
 						// Проверьте каждый угол хит тайла, сохраняя расстояние от
 						// игрока и вычисленное скалярное произведение двух лучей
 						for (int tx = 0; tx < 2; tx++) {
 							for (int ty = 0; ty < 2; ty++) {
-								// Угол от угла к глазу
+								// Угол от угла
 								float vy = (float)nTestY + ty - fPlayerY;
 								float vx = (float)nTestX + tx - fPlayerX;
 								float d = sqrt(vx * vx + vy * vy);
@@ -142,13 +143,20 @@ signed main() {
 						}
 
 						// Сортировка пар от ближайшей к самой дальней
-						sort(p.begin(), p.end(), [](const pair<float, float>& left, const pair<float, float>& right) {return left.first < right.first; });
+						sort(p.begin(), p.end(), [](const pair<float, float> &left, const pair<float, float> &right) {return left.first < right.first;});
 
 						// Первые два/три самые близкие (мы никогда не увидим все четыре)
-						float fBound = 0.01;
-						if (acos(p.at(0).second) < fBound) bBoundary = true;
-						if (acos(p.at(1).second) < fBound) bBoundary = true;
-						if (acos(p.at(2).second) < fBound) bBoundary = true;
+						float fBound = 0.01f;
+
+						if (acos(p.at(0).second) < fBound) {
+							bBoundary = true;
+						}
+						if (acos(p.at(1).second) < fBound) {
+							bBoundary = true;
+						}
+						if (acos(p.at(2).second) < fBound) {
+							bBoundary = true;
+						}
 					}
 				}
 			}
@@ -161,60 +169,62 @@ signed main() {
 			short nShade = ' ';
 
 			if (fDistanceToWall <= fDepth / 4.0f) {
-				nShade = 0x2588; // Очень близко
-			}
-			else if (fDistanceToWall < fDepth / 3.0f) {
+				nShade = 0x2588;
+			} else if (fDistanceToWall < fDepth / 3.0f) {
 				nShade = 0x2593;
-			}
-			else if (fDistanceToWall < fDepth / 2.0f) {
+			} else if (fDistanceToWall < fDepth / 2.0f) {
 				nShade = 0x2592;
-			}
-			else if (fDistanceToWall < fDepth) {
+			} else if (fDistanceToWall < fDepth) {
 				nShade = 0x2591;
-			}
-			else {
-				nShade = ' '; // Слишком далеко
+			} else {
+				nShade = ' ';
 			}
 			if (bBoundary) {
-				nShade = ' '; // Черный
+				nShade = ' ';
 			}
 
 			for (int y = 0; y < nScreenHeight; y++) {
 				// Каждый ряд
 				if (y <= nCeiling) {
 					screen[y * nScreenWidth + x] = ' ';
-				}
-				else if (y > nCeiling && y <= nFloor) {
+				} else if (y > nCeiling && y <= nFloor) {
 					screen[y * nScreenWidth + x] = nShade;
-				}
-				else { // Пол
+				} else {
 					// Затенение пола в зависимости от расстояния
 					float b = 1.0f - (((float)y - nScreenHeight / 2.0f) / ((float)nScreenHeight / 2.0f));
-					if (b < 0.25)		nShade = '#';
-					else if (b < 0.5)	nShade = 'x';
-					else if (b < 0.75)	nShade = '.';
-					else if (b < 0.9)	nShade = '-';
-					else				nShade = ' ';
+
+					if (b < 0.6) {
+						nShade = '#';
+					} else if (b < 0.7) {
+						nShade = 'x';
+					} else if (b < 0.8) {
+						nShade = '-';
+					} else if (b < 0.9) {
+						nShade = '.';
+					} else {
+						nShade = ' ';
+					}
 					screen[y * nScreenWidth + x] = nShade;
 				}
 			}
 		}
 
-		// Показать статистику
-		swprintf_s(screen, 40, L"X=%3.2f, Y=%3.2f, A=%3.2f FPS=%3.2f ", fPlayerX, fPlayerY, fPlayerA, 1.0f / fElapsedTime);
+		swprintf_s(screen, 50, L"X=%3.2f, Y=%3.2f, A=%3.2f, S=%3.2f, FPS=%3.2f ", fPlayerX, fPlayerY, fPlayerA, fSpeed, 1.0f / fElapsedTime);
 
 		// Показать карту
-		for (int nx = 0; nx < nMapWidth; nx++) {
+		for (int nx = 0; nx < nMapWidth; nx++)
 			for (int ny = 0; ny < nMapWidth; ny++) {
 				screen[(ny + 1) * nScreenWidth + nx] = map[ny * nMapWidth + nx];
 			}
-		}
 		screen[((int)fPlayerX + 1) * nScreenWidth + (int)fPlayerY] = 'P';
 
 		// Рамка дисплея
 		screen[nScreenWidth * nScreenHeight - 1] = '\0';
-		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0, 0 }, &dwBytesWritten);
-	}
+		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, {0, 0}, &dwBytesWritten);
 
-	return 0;
+		if (fPlayerX < 0.0) {
+			cout << endl << "Поздравляю вы прошли..." << endl;
+			return 0;
+		}
+	}
 }
